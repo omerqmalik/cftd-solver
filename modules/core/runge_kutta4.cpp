c318497177751f789c7f9be5da63de92aad5931d
Tanay <tanay@Suparnas-MacBook-Pro.local>
2017-07-23 23:33:54 -0400

    using ublas to replicate MatLab code

--- modules/core/runge_kutta4.cpp
+++ modules/core/runge_kutta4.cpp
@@ -21,10 +21,22 @@ double k_a = 0.0;
 const std::complex<double> halfI = std::complex<double>(0, 0.5);
 std::complex<double> n;
 
-vector <double> pump_pwr(100);
-vector <double> noise_vec(100);
+vector <double> pump_pwr;
+vector <double> noise_vec;
 
-state_type CFvals(1000);
+state_type CFvals;
+
+state_type a_vec;
+state_type b_vec1;
+state_type b_vec2;
+state_type Q;
+state_type NL_term;
+state_type aD_vec;
+state_type D_vec;
+state_type holder;
+state_type bB_vec;
+
+matrix<std::complex<double>> D_mat;
 
 matrix <double> A;
 matrix <double> B;
@@ -32,248 +44,140 @@ matrix <double> outT;
 
 matrix<std::complex<double>> outY;
 
+typedef runge_kutta4<vector<double>> rk4;
+
 enum solver_type {FP, RING, UCF};
 
-void initialize(const state_type& y, state_type& a_vec, state_type& b_vec1, state_type& b_vec2, state_type& NL_term, 
-                state_type& aD_vec, state_type& D_vec, state_type& holder, 
-                matrix<std::complex<double>>& D_mat, state_type& Q,  state_type& bB_vec, solver_type st)
-{
-    std::cout << "beginning of init" << std::endl;
-    
-    for(int i = 0; i < N; i++) {
-        a_vec[i] = y[i];
-        b_vec1[i] = y[i + N];
-        b_vec2[i] = std::conj(y[i + N]);
-    }
-    
-    std::cout << "after loop 1" << std::endl;
-
-    for(int i = 0; i < N; i++) {
-        
-        for(int j = 0; j < N; j++) {
-            std::cout << "beginning of inner loop 1" << std::endl;
-            
-            const std::complex<double> & x = y[((i + 2) * N) + j];
-            
-            std::cout << "before matrix assignment" << std::endl;
-            
-            D_mat.insert_element(i, j, x);
-            D_vec[(i * N) + j] = x;
-            
-            const std::complex<double> & c = b_vec2[i] * a_vec[j];
-            const std::complex<double> & d = b_vec2[j] * a_vec[i];
-            
-            std::cout << "before enum check" << std::endl;
-            
-            if(st == FP) {
-                Q[(i * N) + j] = c.imag();
-            }
-
-            else {
-                Q[(i * N) + j] = (c - d); //RING and UCF
-            }
-
-            if (st == UCF) {
-                bB_vec[i] += b_vec1[j] * B(j, i); //just UCF
-            }
-        }
-        
-        
-        CFvals(i) = a_vec[i] * (k_a - ((CFvals(i) * CFvals(i)) / k_a)) * (halfI);
-        
-        
-        
-        for(int j = 0; j < N; j++) {
-            aD_vec[i] += a_vec[j] * D_mat.at_element(j, i);
-        }
-    }
-    
-    std::cout << "after main loop" << std::endl;
-    
-    for(int i = N; i < N + N; i++) {
-        holder[i] = (-1 * g_per * b_vec1[i - N]) - (halfI * 2 * g_per * aD_vec[i - N]);
-    }
-    
-    std::cout << "before last step" << std::endl;
-    
-    if (st == UCF) {
-        
-        for(int i = 0; i < N * N; i++) {
-            for(int j = 0; j < N * N; j++) {
-                NL_term[i] += A.at_element(i, j) * Q[j];
-            }
-        }
-
-
-        for(int i = 0; i < N; i++) {
-            holder[i] = CFvals(i) + (halfI * k_a * bB_vec[i]);
-        }
-
-
-        for(int i = N + N; i < N * (N + 2); i++) {
-            holder[i] = (-1 * g_par * (D_vec[i - (N + N)] - pump_pwr[i - (N + N)]))
-                        + (2 * halfI * g_par * NL_term[i - (N + N)]);
-        }
-        
-    } else if (st == RING) {
-        
-        for(int i = 0; i < N * N; i++) {
-            for(int j = 0; j < N * N; j++) {
-                NL_term[i] += A.at_element(i, j) * Q[j];
-            }
-        }
-
-        for(int i = 0; i < N; i++) {
-            CFvals(i) = a_vec[i] * (k_a - ((CFvals(i) * CFvals(i)) / k_a)) * (halfI);
-            holder[i] = CFvals(i) + (halfI * k_a * b_vec1[i] / (n * n));
-        }
-
-        for(int i = N + N; i < N * (N + 2); i++) {
-            holder[i] = (-1 * g_par * (D_vec[i - (N + N)] - pump_pwr[i - (N + N)]))
-                        + (halfI * g_par * NL_term[i - (N + N)]);
-        }
-        
-    } else if (st == FP) {
-        
-        for(int i = 0; i < N; i++) {
-            holder[i] = CFvals(i) + (halfI * k_a * b_vec1[i] / (n * n));
-        }
-
-        for(int i = 0; i < N * N; i++) {
-            for(int j = 0; j < N * N; j++) {
-                NL_term[i] += A.at_element(i, j) * Q[j];
-            }
-        }
-
-        for(int i = N + N; i < N * (N + 2); i++) {
-            holder[i] = (-1 * g_par * (D_vec[i - (N + N)] - pump_pwr[i - (N + N)]))
-                        + (-1 * g_par * NL_term[i - (N + N)]);
-        }
-    }
-    
-}
-
-void TDSSolvers_FP(const state_type &y, const state_type &dy, const double t)
-{
-    std::cout << "start of fp solver" << std::endl;
-    
-    state_type a_vec;
-    state_type b_vec1;
-    state_type b_vec2;
-    state_type Q;
-    state_type NL_term;
-    state_type aD_vec;
-    state_type D_vec;
-    state_type holder;
-    state_type bB_vec; //unused
-    matrix<std::complex<double>> D_mat;
-
-    holder.resize(N * (N + 2));
-    a_vec.resize(N);
-    b_vec1.resize(N);
-    b_vec2.resize(N);
-    D_mat.resize(N, N);
-    D_vec.resize(N * N);
-    aD_vec.resize(N);
-    Q.resize(N * N);
-    NL_term.resize(N * N);
-    solver_type st = FP;
-    
-    initialize(y, a_vec,  b_vec1, b_vec2,  NL_term, aD_vec, D_vec, holder, D_mat, Q, bB_vec, st);
-    
-    * (state_type *) &dy = holder;
-}
-
-
-void TDSSolvers_UCF(const state_type &y, const state_type &dy, const double t)
-{
-    std::cout << "start of ucf solver" << std::endl;
-    
-    state_type a_vec;
-    state_type b_vec1;
-    state_type b_vec2;
-    state_type Q;
-    state_type NL_term;
-    state_type aD_vec;
-    state_type D_vec;
-    state_type holder;
-    state_type bB_vec;
-    matrix<std::complex<double>> D_mat;
-    //std::cout << "after setup" << std::endl;
-    bB_vec.resize(N);
-    holder.resize(N * (N + 2));
-    a_vec.resize(N);
-    b_vec1.resize(N);
-    b_vec2.resize(N);
-    D_mat.resize(N, N);
-    D_vec.resize(N * N);
-    aD_vec.resize(N);
-    Q.resize(N * N);
-    NL_term.resize(N * N);
-    solver_type st = UCF;
-    //std::cout << "after resizing" << std::endl;
-    initialize(y, a_vec, b_vec1, b_vec2, NL_term, aD_vec, D_vec, holder, D_mat, Q, bB_vec, st);
-    
-    * (state_type *) &dy = holder;
-    std::cout << "after final" << std::endl;
-}
-
-void TDSSolvers_RING(const state_type &y, const state_type &dy, const double t)
-{
-    std::cout << "start of ring solver" << std::endl;
-    
-    state_type a_vec;
-    state_type b_vec1;
-    state_type b_vec2;
-    state_type Q;
-    state_type NL_term;
-    state_type aD_vec;
-    state_type D_vec;
-    state_type holder;
-    state_type bB_vec; //unused
-    matrix<std::complex<double>> D_mat;
-    //std::cout << "after setup" << std::endl;
-    holder.resize(N * (N + 2));
-    a_vec.resize(N);
-    b_vec1.resize(N);
-    b_vec2.resize(N);
-    D_mat.resize(N, N);
-    D_vec.resize(N * N);
-    aD_vec.resize(N);
-    Q.resize(N * N);
-    NL_term.resize(N * N);
-    solver_type st = RING;
-    initialize(y, a_vec,  b_vec1, b_vec2, NL_term, aD_vec, D_vec, holder, D_mat, Q, bB_vec, st);
-    
-    * (state_type *) &dy = holder;
-    
-    std::cout << "after final" << std::endl;
-}
 
 void observer(const state_type &x, const double t)
 {
-    std::cout << "beginning of observer, mindex = " << mindex << std::endl;
-    //std::cout << "t = " << t << std::endl;
-    outT.resize(std::max(outT.size1(), (unsigned long)(mindex + 100) * 2), 1);
+    //std::cout << "beginning of observer, mindex = " << mindex << std::endl;
     outT(mindex, 0) = t;
-    //std::cout << "before loop, N = " << N << std::endl;
-    outY.resize(std::max(outY.size1(), (unsigned long)(mindex + 100) * 2), N * (N + 2));
 
     for(int j = 0; j < N * (N + 2); j++) {
         //std::cout << "j = " << j << " x[j] = " << x[j] << std::endl;
         outY(mindex, j) = x[j];
     }
 
+    /*
+    matrix_row<matrix<std::complex<double> > > mr(outY, mindex);
+    std::copy(x.begin(), x.end(), mr.begin());
+    */
     mindex++;
-    
-    std::cout << "after observer" << std::endl;
+    //std::cout << "after observer" << std::endl;
 }
+
+struct TDSSolvers_RING {
+    void operator () (const state_type& y, const state_type& dy, const double t) const
+    {
+        //a_vec  = y(1:N);
+        a_vec = subrange(y, 0, N);
+        b_vec1 = subrange(y, N, 2 * N);
+        D_vec = subrange(y, (2 * N + 1), y.size());
+
+        matrix<std::complex<double>> Q = outer_prod(conj(b_vec1), a_vec);
+
+        for (int i = 0; i < N * N; i++) {
+            for (int j = 0; j < N * N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                NL_term[i] += A(i, j) * (Q(a, b) - Q(b, a));
+            }
+        }
+
+        state_type a_vecTD_mat(N); // = (a_vec.' * D_mat).'
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                a_vecTD_mat[i] += D_vec(i *(N - 1) + j) * a_vec(i);
+            }
+        }
+
+        scalar_vector<std::complex<double>> k_avec(a_vec.size(), k_a);
+        *(state_type*)&dy = element_prod((k_avec - (element_prod(CFvals, CFvals) / k_a)), (halfI * a_vec)) + (halfI * k_a * b_vec1 / n*n) 
+            - (g_per * b_vec1 - halfI * 2 * g_per * a_vecTD_mat) 
+            - (g_par * (D_vec - pump_pwr) + halfI * g_par * NL_term);
+    }
+};
+
+struct TDSSolvers_UCF {
+    void operator () (const state_type& y, const state_type& dy, const double t) const
+    {
+        a_vec = subrange(y, 0, N);
+        b_vec1 = subrange(y, N, 2 * N);
+        D_vec = subrange(y, (2 * N + 1), y.size());
+
+        matrix<std::complex<double>> Q = outer_prod(conj(b_vec1), a_vec);
+
+        for (int i = 0; i < N * N; i++) {
+            for (int j = 0; j < N * N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                NL_term[i] += A(i, j) * (Q(a, b) - Q(b, a));
+            }
+        }
+
+        state_type a_vecT_D_mat(N);
+        state_type b_vecT_B(N);
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                a_vecT_D_mat[i] += D_vec(i *(N - 1) + j) * a_vec(i);
+                b_vecT_B[i] += B(i,j) * a_vec(i);
+            }
+        }
+
+        scalar_vector<std::complex<double>> k_avec(a_vec.size(), k_a);
+        *(state_type*)&dy = element_prod((k_avec - (element_prod(CFvals, CFvals) / k_a)), (halfI * a_vec)) +
+            (halfI * k_a * b_vecT_B) -
+            (g_per * b_vec1 - halfI * 2 * g_per * a_vecT_D_mat) -
+            (g_par * (D_vec - pump_pwr) + 2 * halfI * g_par * NL_term);
+    }
+};
+
+struct TDSSolvers_FP {
+    void operator () (const state_type& y, const state_type& dy, const double t) const
+    {
+        a_vec = subrange(y, 0, N);
+        b_vec1 = subrange(y, N, 2*N);
+        D_vec = subrange(y, (2*N+1), y.size());
+
+        matrix<std::complex<double>> Q = imag(outer_prod(conj(b_vec1), a_vec));
+
+        for (int i = 0; i < N * N; i++) {
+            for (int j = 0; j < N * N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                NL_term[i] += A(i, j) * Q(a, b);
+            }
+        }
+
+        state_type a_vecTD_mat(N); // = (a_vec.' * D_mat).'
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                int a = j / N;
+                int b = j % N - 1;
+                a_vecTD_mat[i] += D_vec(i *(N-1) + j) * a_vec(i);
+            }
+        }
+
+        scalar_vector<std::complex<double>> k_avec(a_vec.size(), k_a);
+        *(state_type*)&dy = element_prod((k_avec - (element_prod(CFvals, CFvals) / k_a)), (halfI * a_vec)) + 
+            (halfI * k_a * b_vec1 / n*n) - 
+            (g_per * b_vec1 - halfI * 2 * g_per * a_vecTD_mat) - 
+            (g_par * (D_vec - pump_pwr) - g_par * NL_term);
+    }
+};
 
 void mexFunction(int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
-    std::cout << "beginning of mexfunction" << std::endl;
-    
+    //std::cout << "beginning of mexfunction" << std::endl;
     g_per = *mxGetPr(mxGetField(prhs[0], 0, "g_per"));
     g_par = *mxGetPr(mxGetField(prhs[0], 0, "g_par"));
     k_a = *mxGetPr(mxGetField(prhs[0], 0, "k_a"));
@@ -312,36 +216,51 @@ void mexFunction(int nlhs, mxArray *plhs[],
     auto t_initial_pt = mxGetPr(prhs[2]);
     auto t_final_pt = mxGetPr(prhs[3]);
     mindex = 0;
-    outT.resize(10000, 1);
-    outY.resize(10000, N * (N + 2));
 
-    std::cout << "before integrate" << std::endl;
-    
-    std::cout << "basis_type: " << basis_type << std::endl;
+    std::cout << "Resizing all structures" << std::endl;
+
+    const int dt_steps = 5089;
+    double dt = (*t_final_pt - *t_initial_pt) / dt_steps;
+    outT.resize(dt_steps + 1, 1);
+    outY.resize(dt_steps + 1, N * (N + 2));
+    holder.resize(N * (N + 2));
+    a_vec.resize(N);
+    b_vec1.resize(N);
+    b_vec2.resize(N);
+    D_mat.resize(N, N);
+    D_vec.resize(N * N);
+    aD_vec.resize(N);
+    Q.resize(N * N);
+    NL_term.resize(N * N);
 
     if(basis_type.compare("RING") == 0) {
-        integrate(TDSSolvers_RING, noise_vec, *t_initial_pt, *t_final_pt, 0.050, observer);
+        std::cout << "Before calling RING integrator" << std::endl;
+        integrate_const(rk4(), TDSSolvers_RING(), noise_vec, *t_initial_pt, *t_final_pt, dt, observer);
     }
 
     else if(basis_type.compare("UCF") == 0) {
-        integrate(TDSSolvers_UCF, noise_vec, *t_initial_pt, *t_final_pt, 0.050, observer);
+        std::cout << "Before calling UCF integrator" << std::endl;
+        integrate_const(rk4(), TDSSolvers_UCF(), noise_vec, *t_initial_pt, *t_final_pt, dt, observer);
     }
 
     else if(basis_type.compare("FP") == 0) {
-        integrate(TDSSolvers_FP, noise_vec, *t_initial_pt, *t_final_pt, 0.050, observer);
+        std::cout << "Before calling FP integrator" << std::endl;
+        integrate_const(rk4(), TDSSolvers_FP(), noise_vec, *t_initial_pt, *t_final_pt, dt, observer);
     }
 
     else {
         exit(EXIT_FAILURE);
     }
 
+    //outT.resize (mindex, 1);
+    //outY.resize (mindex, N*(N+2));
     std::cout << "after integrate" << std::endl;
-    std::cout << "before assigning output" << std::endl;
+    //std::cout << "before assigning output" << std::endl;
     plhs[0] = mxCreateDoubleMatrix((mwSize) outT.size1(), (mwSize) outT.size2(), mxREAL);
-    std::cout << "before setting field 1" << std::endl;
+    //std::cout << "before setting field 1" << std::endl;
     double* outputMatrix1 = (double *)mxGetData(plhs[0]);
 
-    std::cout << "before output assignment loop" << std::endl;
+    //std::cout << "before output assignment loop" << std::endl;
 
     for(int col = 0; col < outT.size2(); col++) {
         for(int row = 0; row < outT.size1(); row++) {
@@ -352,7 +271,7 @@ void mexFunction(int nlhs, mxArray *plhs[],
         }
     }
 
-    std::cout << "in the middle of assigning output" << std::endl;
+    //std::cout << "in the middle of assigning output" << std::endl;
     plhs[1] = mxCreateDoubleMatrix((mwSize) outY.size1(), (mwSize) outY.size2(), mxCOMPLEX);
     double * opReal = (double *) mxGetPr(plhs[1]);
     double * opImag = (double *) mxGetPi(plhs[1]);
